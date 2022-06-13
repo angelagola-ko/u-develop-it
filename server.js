b98f37f862827678d61a7da75129e9454b0f3287
Angela Gola <angelagola.ko@gmail.com>
2022-06-13 16:30:28 -0400

    connected mySQL

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+
+const mysql = require('mysql2');
 const express = require("express");
 const PORT = process.env.PORT || 3030;
 const app = express();
@@ -6,6 +8,18 @@ const app = express();
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
+//Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //mySQL uername,
+        user: 'root',
+        //mySQL pw
+        password: 'Ecostar45*',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 // app.get('/',(req,res) => {
 //     res.json({
 //         message: 'Hello World'
