d60743cef572cd8f5a1bbc94fcc8c07eb11de485
Angela Gola <angelagola.ko@gmail.com>
2022-06-15 14:20:03 -0400

    added queries for parties in server.js

--- server.js
+++ server.js
@@ -23,9 +23,65 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
 );
 
+app.get('/api/parties', (req,res) => {
+    const sql = `SELECT * FROM parties`;
+    db.query(sql, (err,rows) => {
+        if(err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json ({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+app.get('/api/party/:id', (req,res) => {
+    const sql = `SELECT * FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err,row) => {
+        if(err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json ({
+            message: 'success',
+            data:row
+        });
+    });
+});
+
+app.delete('/api/party/:id', (req,res) => {
+    const sql = `DELETE FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: res.message });
+            //checks if anything was deleted
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Party not found.'
+            });
+        } else {
+            res.json ({
+                message: 'deleted',
+                changes: result.affectedRows,
+                id: req.params.id
+            });
+        }
+    });
+});
+
 //get all candidates
 app.get('/api/candidates', (req,res) => {
-    const sql = `SELECT * FROM candidates`;
+    //const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.name
+                AS party_name
+                FROM candidates
+                LEFT JOIN parties
+                ON candidates.party_id = parties.id`;
+
     db.query(sql, (err,rows) => {
         if(err) {
             res.status(500).json({ error: err.message });
@@ -44,7 +100,14 @@ app.get('/api/candidates', (req,res) => {
     
     //get a single candidate
     app.get('/api/candidate/:id', (req,res) => {
-        const sql = `SELECT * FROM candidates WHERE id = ?`;
+      //  const sql = `SELECT * FROM candidates WHERE id = ?`;
+        const sql = `SELECT candidates.*, parties.name
+                    AS party_name
+                    FROM candidates
+                    LEFT JOIN parties
+                    ON candidates.party_id = parties.id
+                    WHERE candidates.id = ?`;
+
         const params = [req.params.id];
     
         db.query(sql, params, (err,row) => {
